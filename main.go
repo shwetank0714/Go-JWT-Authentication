0e88b473cb347c3ca66b7090dfac47d199e6271d
shwetank0714 <shwetank.dwivedi@rara.delivery>
2023-05-23 23:48:25 +0530

    hadnler func for login and home

--- main.go
+++ main.go
@@ -9,8 +9,6 @@ func main(){
 
 		http.HandleFunc("/login",LoginHandler)
 		http.HandleFunc("/home",HomeHandler)
-		http.HandleFunc("/refresh",RefreshHandler)
-
 		log.Fatal(http.ListenAndServe(":8080",nil))
 		
 
